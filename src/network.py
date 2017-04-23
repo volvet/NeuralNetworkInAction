de6bd195fba8db651fd0c9d67c86d701ee1200f1
volvet <volvet@163.com>
2017-04-23 13:31:25 +0800

    learn process

--- src/network.py
+++ src/network.py
@@ -37,9 +37,23 @@ class Network(object):
 		return
 
 	def update_mini_batch(self, mini_batch, eta):
+		nabla_b = [np.zeors(b.shape) for b in self.biases]
+		nabla_w = [np.zeros(w.shape) for w in self.weights]
+		for x, y in mini_batch:
+			delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+			nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+			nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+		self.weights = [w - (eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
+		self.biases = [b - (eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
 		return
 	
 	def backprop(self, x, y):
+		return
+
+	def evaluate(self, test_data):
+		return
+
+	def cost_derivative(self, output_activations, y):
 		return
 
 if __name__ == '__main__':
