e6f9b264bf5199734662659366d4ce07e4968284
volvet <volvet@163.com>
2017-05-13 18:46:17 +0800

    reformat source

--- src/network.py
+++ src/network.py
@@ -1,66 +1,68 @@
-
 import numpy as np
+import random
 
 
 def sigmoid(z):
-	return 1.0/(1.0+np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
+
 
 def sigmoid_prime(z):
-	return sigmoid(z)/(1-sigmoid(z))
+    return sigmoid(z) / (1 - sigmoid(z))
+
 
 class Network(object):
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+        return
 
-	def __init__(self, sizes):
-		self.num_layers = len(sizes)
-		self.sizes = sizes
-		self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-		self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-		return
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a) + b)
+        return a
 
-	def feedforward(self, a):
-		for b, w in zip(self.biases, self.weights):
-			a = sigmoid(np.dot(w, a) + b)
-		return a
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in xrange(epochs):
+            random.shuffle(training_data)
+            mini_batches = [training_data[k: k + mini_batch_size] for k in xrange(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print "Epoch {0}:{1}/{2}".format(j, self.evaluate(test_data), n_test)
+            else:
+                print "Epoch {0} complete".format(j)
+        return
 
-	def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
-		if test_data: n_test = len(test_data)
-		n = len(training_data)
-		for j in xrange(epochs):
-			random.shuffle(training_data)
-			mini_batches = [training_data[k: k+mini_batch_size] for k in xrange(0, n, mini_batch_size)]
-			for mini_batch in mini_batches:
-			    self.update_mini_batch(mini_batch, eta)
-			if test_data:
-			    print "Epoch {0}:{1}/{2}".format(j, self.evaluate(test_data), n_test)
-			else:
-				print "Epoch {0} complete".format(j)
-		return
+    def update_mini_batch(self, mini_batch, eta):
+        nabla_b = [np.zeors(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
+        return
 
-	def update_mini_batch(self, mini_batch, eta):
-		nabla_b = [np.zeors(b.shape) for b in self.biases]
-		nabla_w = [np.zeros(w.shape) for w in self.weights]
-		for x, y in mini_batch:
-			delta_nabla_b, delta_nabla_w = self.backprop(x, y)
-			nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
-			nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-		self.weights = [w - (eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
-		self.biases = [b - (eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
-		return
-	
-	def backprop(self, x, y):
-		return
+    def backprop(self, x, y):
+        return
 
-	def evaluate(self, test_data):
-		return
+    def evaluate(self, test_data):
+        return
 
-	def cost_derivative(self, output_activations, y):
-		return
+    def cost_derivative(self, output_activations, y):
+        return
+
 
 if __name__ == '__main__':
-	print 'test network'
-	sizes = [2, 3, 1]
-	net = Network(sizes)
-	print "sizes: ", sizes
-	print "biases: ", net.biases
-	print "weights: ", net.weights
-	print "networks: ", zip(net.biases, net.weights)
+    print 'test network'
+    sizes = [2, 3, 1]
+    net = Network(sizes)
+    print "sizes: ", sizes
+    print "biases: ", net.biases
+    print "weights: ", net.weights
+    print "networks: ", zip(net.biases, net.weights)
